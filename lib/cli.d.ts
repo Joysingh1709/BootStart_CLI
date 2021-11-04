c9c65c9387bfba8c340be25bc462aaa189d3d135
Joy Singh <avnishmamta060606@gmail.com>
2021-11-05 03:25:32 +0530

    dsv

--- lib/cli.d.ts
+++
@@ -1,8 +0,0 @@
-export function parseArgumentsIntoOptions(rawArgs: any): {
-    skipPrompts: boolean;
-    git: boolean;
-    template: string;
-    runInstall: boolean;
-};
-export function promptForMissingOptions(options: any): Promise<any>;
-export function cli(args: any): Promise<void>;
