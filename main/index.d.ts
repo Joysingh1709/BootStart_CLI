15796dfd3d2bc11524ae6a1adafc0f3160b254ae
Joy Singh <avnishmamta060606@gmail.com>
2021-11-05 03:29:38 +0530

    asc

--- main/index.d.ts
+++
@@ -1,1 +0,0 @@
-export function drawBs(): Promise<void>;
--- main/index.js
+++
@@ -1,1 +0,0 @@
-"use-strict";import chalk from'chalk';import figlet from'figlet';console.clear();export async function drawBs(){console.log(chalk.yellow(figlet.textSync('BootStart',{horizontalLayout:'full'})));}
